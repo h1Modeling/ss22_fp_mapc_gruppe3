f1350f54c1dfce4ebd2ba3c82b357e1bd11387dd
Heinz Stadler <info@h1-im.de>
2022-04-13 20:24:20 +0200

    create template project

---
+++ src/main/java/de/feu/massim22/group3/agents/BasicAgent.java
@@ -0,0 +1,48 @@
+package de.feu.massim22.group3.agents;
+
+import eis.iilang.*;
+
+import java.util.List;
+
+import de.feu.massim22.group3.MailService;
+
+/**
+ * A very basic agent.
+ */
+public class BasicAgent extends Agent {
+
+    private int lastID = -1;
+
+    /**
+     * Constructor.
+     * @param name    the agent's name
+     * @param mailbox the mail facility
+     */
+    public BasicAgent(String name, MailService mailbox) {
+        super(name, mailbox);
+    }
+
+    @Override
+    public void handlePercept(Percept percept) {}
+
+    @Override
+    public void handleMessage(Percept message, String sender) {}
+
+    @Override
+    public Action step() {
+        List<Percept> percepts = getPercepts();
+        for (Percept percept : percepts) {
+            if (percept.getName().equals("actionID")) {
+                Parameter param = percept.getParameters().get(0);
+                if (param instanceof Numeral) {
+                    int id = ((Numeral) param).getValue().intValue();
+                    if (id > lastID) {
+                        lastID = id;
+                        return new Action("move", new Identifier("n"));
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
