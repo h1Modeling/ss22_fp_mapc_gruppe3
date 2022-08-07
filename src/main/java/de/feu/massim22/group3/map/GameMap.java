b342593731c276245a54fa6d414f16888eb0023d
Heinz Stadler <info@h1-im.de>
2022-08-07 09:24:05 +0200

    improve project site

--- src/main/java/de/feu/massim22/group3/map/GameMap.java
+++ src/main/java/de/feu/massim22/group3/map/GameMap.java
@@ -23,7 +23,7 @@ import org.lwjgl.BufferUtils;
 public class GameMap {
     
     private Point initialSize;
-    private Point size = new Point(64, 92); // null;
+    private Point size = new Point(300, 20); // null;
     // First dimension are rows, second dimension are columns
     private MapCell[][] cells;
     private Point topLeft; // top left indices can be negative
