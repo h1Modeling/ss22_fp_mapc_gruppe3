d22a35891a82288b790d78f0b71087720ebf8ff9
Heinz Stadler <info@h1-im.de>
2022-09-27 18:11:15 +0200

    add final javaDoc

---
+++ src/test/java/de/feu/massim22/group3/agents/desires/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides classes which test desires of an agent.
+ */
+package de.feu.massim22.group3.agents.desires;
---
+++ src/test/java/de/feu/massim22/group3/map/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides classes for testing the map data structure.
+ */
+package de.feu.massim22.group3.map;
